c71f3c2bf2aeebb69f9d21e328750948e0bedc4d
jaksiri <jakrinsiri@hotmail.com>
2023-07-25 01:18:10 -0400

    fix: scrolling event fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,68 +3,29 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import PageWrapper from "@/components/PageWrapper";
 import SlidingHeader from "@/components/SlidingHeader";
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [scrollPos, setScrollPos] = useState(0);
+
   function handleScroll() {
     setScrollPos(window.scrollY);
     console.log(scrollPos);
   }
-  window.onscroll = () => {
-    handleScroll();
-  };
-
-  // const onScroll = useCallback((e: any) => {
-  //   const { scrollY } = window;
-  //   console.log(scrollY);
-  // }, []);
-  // useEffect(() => {
-  //   console.log("scroll pos: " + scrollPos);
-  //   console.log(document.scrollingElement);
-  //   window.addEventListener("scroll", onScroll, false);
-  //   return window.removeEventListener("scroll", onScroll);
-  // }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
   return (
-    <PageWrapper>
-      <div className={styles.main}>
-        <SlidingHeader />
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-        <section className={styles.container}>
-          <Link href="/project-gallery">
-            <div className={styles.pagelinks}>Project Gallery</div>
-          </Link>
-        </section>
-      </div>
-    </PageWrapper>
+    <div className={styles.main}>
+      <SlidingHeader />
+      <section className={styles.container}>
+        <Link href="/project-gallery">
+          <div className={styles.pagelinks}>Project Gallery</div>
+        </Link>
+      </section>
+    </div>
   );
 }
